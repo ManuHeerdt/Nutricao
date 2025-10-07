4b93ab04ba9b912be30f6113cd9c78705e19f209
Manu Heerdt <emanuelly.heerdt@escola.pr.gov.br>
2025-10-07 09:23:25 -0300

    uytu

--- js/principal.js
+++ js/principal.js
@@ -3,7 +3,7 @@ console.log(titulo);
 console.log(titulo.textContent);
 titulo.textContent = ("Aparecida Nutricionista");
 
-var paciente = document.querySelector(#primeiro-paciente);
+var paciente = document.querySelector("#primeiro-paciente");
 console.log (paciente);
 
 var tdNome = paciente.querySelector("info-nome");
