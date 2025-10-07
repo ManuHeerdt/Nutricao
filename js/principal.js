701c7092507e431fb42a9e55a0db1b995d851c12
Manu Heerdt <emanuelly.heerdt@escola.pr.gov.br>
2025-10-07 09:11:35 -0300

    tiopio

--- js/principal.js
+++ js/principal.js
@@ -7,8 +7,6 @@ var paciente = document.querySelector("#primeiro-paciente");
 console.log(paciente);
 
 var tdPeso = paciente.querySelector(".info-peso");
-console.log(tdPeso);
-
 var peso = tdPeso.textContent;
 console.log(paciente)
 console.log(peso)
