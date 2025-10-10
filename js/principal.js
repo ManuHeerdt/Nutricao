5ea0355f45a1725ed830e394d2676bcfd79b8f31
Manu Heerdt <emanuelly.heerdt@escola.pr.gov.br>
2025-10-10 10:48:45 -0300

    uioyo

--- js/principal.js
+++ js/principal.js
@@ -30,11 +30,11 @@ var alturaEhvalida = true;
 if (peso <0 || peso >1000) {
     console.log("Peso inválido");
     pesoEhValido = false;
-    tdImc.textContent="Peso Inválido";
+    tdImc.textContent="Peso Inválido!";
 }
 
 if (altura < 0 || altura > 3.00){
-    console.log("Altura inválida");
+    console.log("Altura inválida!");
     alturaEhValida = false;
     tdImc.textContent="Altura Inválida!";
 
